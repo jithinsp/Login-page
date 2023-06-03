eb50ec3d6a4aaee9bdc95c703e80b42563470845
jithinsp <jithinsp001@gmail.com>
2023-06-03 15:02:15 +0530

    initial commit of Login page using spring

--- src/main/java/com/login/spring/loginpage/HomeController.java
+++ src/main/java/com/login/spring/loginpage/HomeController.java
@@ -1,12 +1,14 @@
 package com.login.spring.loginpage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message","UserName");
         return "home";
     }
     @GetMapping("/")
